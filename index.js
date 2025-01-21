3365d38572c56f4a4545ddaa7f708c6578b72edf
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-01-21 23:51:27 +0530

    dot env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 //importing the routes:
 const userRoutes = require('./routes/userRoutes');
@@ -11,6 +11,7 @@ const pharmacyRoutes = require('./routes/pharmacyRoutes');
 const app = express();
 app.use(express.json());
 
+//using the routes 1
 app.use("/users", userRoutes);
 app.use("/medicines", medicineRoutes);
 app.use("/pharmacies", pharmacyRoutes);
