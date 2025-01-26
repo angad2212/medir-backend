2d1e0fcb351343841852e8d02b7df6e2682f66a6
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-01-26 06:42:08 +0530

    database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
